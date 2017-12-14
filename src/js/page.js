27f2f7bb37b46eec42531652c96e45889bba1ba2
Linus Willner <lwtechgaming@gmail.com>
2017-12-14 13:40:03 +0200

    Add dynamic page size

--- src/js/page.js
+++ src/js/page.js
@@ -32,4 +32,18 @@ function addSoundItem (filename, filepath) {
   ul.appendChild(li)
 }
 
+function resizeCollections () {
+  let screenSize = _detectWindowSize()
+  html.get('console').setAttribute('style', `height: ${screenSize}px;`)
+  html.get('sounds-list').setAttribute('style', `height: ${screenSize}px;`)
+}
+
+function _detectWindowSize () {
+  let h = window.screen.height
+  let w = window.screen.width
+  if (w > 1366 && h > 768) return 330
+  else if (w <= 1366 && h <= 768) return 290
+}
+
 exports.addSoundItem = addSoundItem
+exports.resizeCollections = resizeCollections
